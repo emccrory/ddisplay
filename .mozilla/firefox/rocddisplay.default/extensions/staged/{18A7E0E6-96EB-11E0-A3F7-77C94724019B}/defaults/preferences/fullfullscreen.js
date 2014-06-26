4b6ec4c50b9ca109200cd85d90daf9c9f6dc5c47
Pat Riehecky <riehecky@fnal.gov>
2014-06-26 14:40:50 -0500

    Added Full Fullscreen 7

---
+++ .mozilla/firefox/rocddisplay.default/extensions/staged/{18A7E0E6-96EB-11E0-A3F7-77C94724019B}/defaults/preferences/fullfullscreen.js
@@ -0,0 +1,4 @@
+pref("extensions.fullfullscreen.start", true);
+pref("extensions.fullfullscreen.noautohide", true);
+pref("extensions.fullfullscreen.notabs", true);
+pref("extensions.fullfullscreen.noscroll", false);
