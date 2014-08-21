afc6bde57c2652e7729e5c5ae86419b7cb67e5dd
Elliott McCrory <mccrory@fnal.gov>
2014-08-21 09:07:43 -0500

    Change the runDisplay.sh script to not require the display number (only the screen number if it is not equal to 0).
    Add new icon for "ChannelSelector" on chat room status display
    
    Update jar and class files

--- DynamicDisplays/runDisplay.sh
+++ DynamicDisplays/runDisplay.sh
@@ -1,10 +1,9 @@
 . setupJars.sh
 
-displayNum=8
+screenNum=0
 if [ "$1 X" != " X" ]; then
-    displayNum=$1;
+    screenNum=$1;
 fi
 
-# java gov.fnal.ppd.signage.display.attic.DisplayAsStandaloneBrowser-display=$displayNum
-java gov.fnal.ppd.signage.display.testing.DisplayAsConnectionToFireFox -display=$displayNum
+java gov.fnal.ppd.signage.display.testing.DisplayAsConnectionToFireFox -screen=$displayNum
 
