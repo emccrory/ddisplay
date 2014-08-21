3204c33372a30b31997119630c712d0f68f5190e
Elliott McCrory <mccrory@fnal.gov>
2014-08-21 09:10:36 -0500

    Add new files

---
+++ DynamicDisplays/src/gov/fnal/ppd/signage/util/JLabelFooter.java
@@ -0,0 +1,38 @@
+package gov.fnal.ppd.signage.util;
+
+import static gov.fnal.ppd.signage.util.Util.shortDate;
+import static gov.fnal.ppd.signage.util.Util.truncate;
+import gov.fnal.ppd.ChannelSelector;
+
+import java.awt.Dimension;
+
+import javax.swing.JLabel;
+
+/**
+ * A single-purpose instance of JLabel to show the status of the Display at the bottom of the ChannelSelector screen Adjusts the
+ * text within the label to fit in the provided space.
+ * 
+ * @author Elliott McCrory, Fermilab AD/Instrumentation
+ * @copy 2014
+ * 
+ */
+public class JLabelFooter extends JLabel {
+
+	private static final long	serialVersionUID	= -7328547318315265510L;
+	private final static int	DEFAULT_SIZE		= (ChannelSelector.SHOW_IN_WINDOW ? 30 : 60);
+
+	public JLabelFooter(String text) {
+		super("<html>" + truncate(text, DEFAULT_SIZE) + " (" + shortDate() + ")" + "</html>");
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void setText(String text) {
+		Dimension d = getSize();
+		int numChars = DEFAULT_SIZE;
+		if (d.height != 0 && d.width != 0)
+			numChars = 3 * d.width / d.height;
+
+		super.setText("<html>" + truncate(text, numChars) + " (" + shortDate() + ")" + "</html>");
+	}
+}
