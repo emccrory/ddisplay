a20c00d4a09e2434139e7fc086256f1b2ab33b66
Elliott McCrory <mccrory@fnal.gov>
2014-09-23 13:40:47 -0500

    Change the messaging server!

--- DynamicDisplays/setupJars.sh
+++ DynamicDisplays/setupJars.sh
@@ -1,3 +1,4 @@
 export CLASSPATH=bin:lib/mysql-connector-java-5.0.3-bin.jar:lib/slf4j-api-1.5.8.jar:lib/slf4j-log4j12-1.5.8.jar
 
-export messagingServer="mccrory.fnal.gov"
+# Messaging server is NUC #11
+export messagingServer="dd-124701.fnal.gov"
