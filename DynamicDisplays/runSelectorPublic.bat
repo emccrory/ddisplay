ef620e4cb690ebb69f713f04d1c7ffa797eeae85
Elliott McCrory <mccrory@fnal.gov>
2014-09-23 13:44:40 -0500

    Change the messaging server to dd-124701 (NUC #11)

--- DynamicDisplays/runSelectorPublic.bat
+++ DynamicDisplays/runSelectorPublic.bat
@@ -3,12 +3,13 @@ set path="c:\Program Files (x86)\Java\jre7\bin";%path%
 set classpath=bin;lib/mysql-connector-java-5.0.3-bin.jar;lib/slf4j-api-1.5.8.jar;lib/slf4j-log4j12-1.5.8.jar
 set "public=true"
 set "loc=0"
-set "server=mccrory.fnal.gov"
+set "databaseServer=mccrory.fnal.gov"
+set "messagingServer=dd-124701.fnal.gov"
 
 java -Dddisplay.selector.inwindow=FALSE^
      -Dddisplay.selector.public=%public% ^
      -Dddisplay.selector.location=%loc% ^
-     -Dddisplay.messagingserver=%server% ^
-     -Dddisplay.dbserver=%server% ^
+     -Dddisplay.messagingserver=%messagingServer% ^
+     -Dddisplay.dbserver=%databaseServer% ^
      -Xmx512m gov.fnal.ppd.ChannelSelector 
 
