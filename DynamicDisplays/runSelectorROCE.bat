17c6fb135c5cdb8ed8ed6a265b32587e56ff7717
Elliott McCrory <mccrory@fnal.gov>
2014-12-05 09:37:21 -0600

    New Windows scripts to run the ChannelSelector in other places.

---
+++ DynamicDisplays/runSelectorROCE.bat
@@ -0,0 +1,15 @@
+set path="c:\Program Files (x86)\Java\jre7\bin";%path%
+
+set classpath=bin;lib/mysql-connector-java-5.0.3-bin.jar;lib/slf4j-api-1.5.8.jar;lib/slf4j-log4j12-1.5.8.jar
+set "public=false"
+set "loc=2"
+set "databaseServer=mccrory.fnal.gov"
+set "messagingServer=roc-w-11.fnal.gov"
+
+java -Dddisplay.selector.inwindow=FALSE^
+     -Dddisplay.selector.public=%public% ^
+     -Dddisplay.selector.location=%loc% ^
+     -Dddisplay.messagingserver=%messagingServer% ^
+     -Dddisplay.dbserver=%databaseServer% ^
+     -Xmx512m gov.fnal.ppd.dd.ChannelSelector 
+
