f811d8aac5f96efe39cffbf3dac8f06a69f1ea0a
Elliott McCrory <mccrory@fnal.gov>
2014-12-10 12:35:15 -0600

    Bug fix -- left out a quote mark

--- DynamicDisplays/config.cmd
+++ DynamicDisplays/config.cmd
@@ -1,4 +1,4 @@
-set path="c:\Program Files (x86)\Java\jre7\bin";"c:\Program Files\Java\jre7\bin;%path%
+set path="c:\Program Files (x86)\Java\jre7\bin";"c:\Program Files\Java\jre7\bin";%path%
 
 set classpath=bin;lib/mysql-connector-java-5.0.3-bin.jar;lib/slf4j-api-1.5.8.jar;lib/slf4j-log4j12-1.5.8.jar
 
