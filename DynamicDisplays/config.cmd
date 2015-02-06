bf0ab3f782fe0acb5724479ce6bb0c065df17205
Elliott McCrory <mccrory@fnal.gov>
2015-02-06 10:02:54 -0600

    Fix bug in the JAR setup files

--- DynamicDisplays/config.cmd
+++ DynamicDisplays/config.cmd
@@ -1,6 +1,6 @@
 set path="c:\Program Files (x86)\Java\jre7\bin";"c:\Program Files\Java\jre7\bin";%path%
 
-set classpath=bin;lib/mysql-connector-java-5.1.27-bin.jar;lib/slf4j-api-1.5.8.jar;lib/slf4j-log4j12-1.5.8.jar
+set classpath=bin;lib/mysql-connector-java-5.1.27.jar;lib/slf4j-api-1.5.8.jar;lib/slf4j-log4j12-1.5.8.jar
 
 set "databaseServer=mccrory.fnal.gov"
 set "messagingServer=roc-w-11.fnal.gov"
--- DynamicDisplays/setupJars.sh
+++ DynamicDisplays/setupJars.sh
@@ -1,4 +1,4 @@
-export CLASSPATH=bin:lib/mysql-connector-java-5.1.27-bin.jar:lib/slf4j-api-1.5.8.jar:lib/slf4j-log4j12-1.5.8.jar
+export CLASSPATH=bin:lib/mysql-connector-java-5.1.27.jar:lib/slf4j-api-1.5.8.jar:lib/slf4j-log4j12-1.5.8.jar
 
 # Messaging server is NUC #11
 export messagingServer="roc-w-11.fnal.gov"
