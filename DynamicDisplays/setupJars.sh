d5eac3e79b9c740a828951b0098657578913c7c2
Elliott McCrory <mccrory@fnal.gov>
2015-05-12 14:12:08 -0500

    Remove old env vars

--- DynamicDisplays/setupJars.sh
+++ DynamicDisplays/setupJars.sh
@@ -1,9 +1,6 @@
 export CLASSPATH=bin:lib/mysql-connector-java-5.1.27.jar:lib/slf4j-api-1.5.8.jar:lib/slf4j-log4j12-1.5.8.jar
 
-# Messaging server is NUC #11
-export messagingServer="roc-w-11.fnal.gov"
-
-# Database server is mccrory
+# Backup database server is mccrory
 export databaseServer="mccrory.fnal.gov"
 
 # Read the database credentials from the secret file
