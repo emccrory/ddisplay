5d40e9385e2868b9c5b2270e9eef511754f35ddf
Elliott McCrory <mccrory@fnal.gov>
2015-06-08 12:09:52 -0500

    Removed unneeded files

--- DynamicDisplays/runFromMac.sh
+++
@@ -1,3 +0,0 @@
-. setupJars.sh
-
-java  gov.fnal.ppd.signage.display.attic.DisplayAsStandaloneBrowser 4231 ff6666 XOC
--- DynamicDisplays/runSelectorWindowROCE.bat
+++
@@ -1,3 +0,0 @@
-REM This is the same script as runSelector.bat now
-
-runSelectorWindow.bat
