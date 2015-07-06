261df523d2c1a49c3244c770692b839f5a337e54
Elliott McCrory <mccrory@fnal.gov>
2015-07-06 13:52:27 -0500

    Catch up with previous commits.

--- DynamicDisplays/newsfeed/getSymmetryFeed.sh
+++ DynamicDisplays/newsfeed/getSymmetryFeed.sh
@@ -1,35 +1,43 @@
 #!/bin/bash
 
-PID=getSymmetryFeed.pid
 
-if [ -a $PID ]; then
-    echo $0 Already running.  Abort.
-    exit -1;
-fi
+(
+  # Wait for lock on $HOME/lock/.temperatureScript.exclusivelock (file desriptor 200) for 2 seconds
+  flock -x -w 2 200 || exit 1
 
-echo $$ > $PID
-
-. /home/mccrory/.bashrc
-
-export CLASSPATH=.:../bin:../lib/mysql-connector-java-5.1.27.jar:../lib/slf4j-api-1.5.8.jar:../lib/slf4j-log4j12-1.5.8.jar
-export PATH=/usr/krb5/bin:/usr/krb5/bin:/usr/lib64/qt-3.3/bin:/usr/local/bin:/usr/bin:/bin:/usr/games:/usr/local/sbin:/usr/sbin:/sbin:/home/mccrory/bin
-
-newsURL="http://www.interactions.org/index.rss"
-
-xmlNewsFile=symfeed.xml
-logNewsFile=symfeed.log
-
-rm -f temp tempN tempW $xmlNewsFile $logNewsFile
-
-/usr/bin/wget -o $logNewsFile -O tempN $newsURL # 2>/dev/null
-
-/bin/sed 's/>/>\n/g' tempN | sed 's-<br/>--g' | sed 's/dc://g' | sed 's/atom://g' | sed 's/media://g' | sed 's/, left,//g' | sed 's/, right,//g' | sed 's/, center,//g' | sed 's/&lt;img .*&gt;//g' > $xmlNewsFile
-
-/usr/bin/java gov.fnal.ppd.dd.xml.news.Channel $xmlNewsFile 10 > symmetry.txt
-
+  . /home/mccrory/.bashrc
+  
+  export CLASSPATH=.:../bin:../lib/mysql-connector-java-5.1.27.jar:../lib/slf4j-api-1.5.8.jar:../lib/slf4j-log4j12-1.5.8.jar
+  export PATH=/usr/krb5/bin:/usr/krb5/bin:/usr/lib64/qt-3.3/bin:/usr/local/bin:/usr/bin:/bin:/usr/games:/usr/local/sbin:/usr/sbin:/sbin:/home/mccrory/bin
+  
+  newsURL="http://www.interactions.org/index.rss"
+  
+  xmlNewsFile=symfeed.xml
+  logNewsFile=symfeed.log
+  
+  rm -f temp tempN tempW $xmlNewsFile $logNewsFile
+  
+  /usr/bin/wget -o $logNewsFile -O tempN $newsURL # 2>/dev/null
+  
+  /bin/sed 's/>/>\n/g' tempN | sed 's-<br/>--g' | sed 's/dc://g' | sed 's/atom://g' | sed 's/media://g' | sed 's/, left,//g' | sed 's/, right,//g' | sed 's/, center,//g' | sed 's/&lt;img .*&gt;//g' > $xmlNewsFile
+  
+  /usr/bin/java gov.fnal.ppd.dd.xml.news.Channel $xmlNewsFile 10 | head -n 15 | sed 's/null//g' | sed 's/--//g' | sed 's/  //g' > symmetry.txt
+  
 # --------------------------------------------------------------------------------------
+  
+  cp symmetry.txt /var/www/html/newsfeed
 
-cp symmetry.txt /var/www/html/newsfeed
-cp symmetry.txt /web/sites/xoc.fnal.gov/htdocs/newsfeed
+  #  if [ -e /web/sites/dynamicdisplays.fnal.gov/htdocs/newsfeed ]; then
+  if /usr/krb5/bin/klist > /tmp/k ; then 
+      cp symmetry.txt /web/sites/dynamicdisplays.fnal.gov/htdocs/newsfeed
+  else
+      echo There seems to be no kerberos ticket, so we cannot copy the newsfeed file to the web directory
+  fi
+  
+) 200>$HOME/lock/.symmetryFeedScript.exclusivelock
 
-rm -f $PID
+rm -f $HOME/lock/.symmetryFeedScript.exclusivelock
+
+
+
+
