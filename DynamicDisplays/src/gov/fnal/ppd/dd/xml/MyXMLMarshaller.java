759a3b4373b72149f13120ad0a92613a7c452ad1
Elliott McCrory <mccrory@fnal.gov>
2015-07-28 16:39:23 -0500

    Catch an "InternalError" exception.

--- DynamicDisplays/src/gov/fnal/ppd/dd/xml/MyXMLMarshaller.java
+++ DynamicDisplays/src/gov/fnal/ppd/dd/xml/MyXMLMarshaller.java
@@ -42,8 +42,27 @@ public class MyXMLMarshaller {
 	}
 
 	public static Object unmarshall(Class<?> clazz, String xml) throws JAXBException {
-		ByteArrayInputStream is = new ByteArrayInputStream(xml.getBytes());
-		return JAXBContext.newInstance(clazz).createUnmarshaller().unmarshal(is);
+		try {
+			ByteArrayInputStream is = new ByteArrayInputStream(xml.getBytes());
+			return JAXBContext.newInstance(clazz).createUnmarshaller().unmarshal(is);
+		} catch (InternalError ie) {
+			/**
+			 * This exception happens in this rare case:
+			 * <ol>
+			 * <li>The URL has not been changed since the JVM started</li>
+			 * <li>The Java version has been upgraded while the JVM was running</li>
+			 * <li>A message is received.</li>
+			 * </ol>
+			 * 
+			 * What happens is that while the underlying code is trying to load the class that has been sent to us, one of the new
+			 * classes it needs to load is supposed to be in a well-known jar file, but that jar file is now missing!
+			 */
+			System.err.println("Horrible exception of type " + InternalError.class.getCanonicalName()
+					+ "!  This probably means that a new version of Java was recently put onto this system.  "
+					+ "Your best bet now is to restart the Display.");
+			ie.printStackTrace();
+		}
+		return null;
 	}
 
 	public static Object unmarshall(Class<?> clazz, File file) throws JAXBException {
