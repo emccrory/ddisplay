fac0c3a060bfb399fc16aa532c1f11c48e72c2d8
Elliott McCrory <mccrory@fnal.gov>
2016-01-15 13:05:10 -0600

    Implement this class so that it plays like a regular channel on a display.  The display must know it is a list, however, so it can ask for the next channel when time runs out.

--- DynamicDisplays/src/gov/fnal/ppd/dd/channel/ChannelPlayList.java
+++ DynamicDisplays/src/gov/fnal/ppd/dd/channel/ChannelPlayList.java
@@ -5,17 +5,14 @@
  */
 package gov.fnal.ppd.dd.channel;
 
-import static gov.fnal.ppd.dd.util.Util.catchSleep;
 import gov.fnal.ppd.dd.changer.ChannelCategory;
 import gov.fnal.ppd.dd.signage.Channel;
-import gov.fnal.ppd.dd.signage.Display;
 import gov.fnal.ppd.dd.signage.SignageContent;
 import gov.fnal.ppd.dd.signage.SignageType;
 
 import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.ListIterator;
 
 /**
  * This class holds a list of Channels that are to be played on a Display
@@ -27,16 +24,16 @@ public class ChannelPlayList implements Channel {
 
 	// FIXME -- No, the playing of a list of channels has to be handled by the Real Display.
 
-	private static final long				serialVersionUID	= 7785042152443699996L;
-	private List<SignageContent>			channels			= new ArrayList<SignageContent>();
-	private ListIterator<SignageContent>	chanIterator;
-	private SignageContent					currentChannel;
-	private long							dwell				= 5000l;
-	private boolean							running				= true;
+	private static final long		serialVersionUID	= 7785042152443699996L;
+	private List<SignageContent>	channels			= new ArrayList<SignageContent>();
+	private int						currentChannel		= 0;
+	private long					dwell				= 5000l;
+	// private boolean running = true;
 	// private Thread playThread = null;
-	private Display							myDisplay			= null;
-	private int								frameNumber			= 0;
-	private long							expiration			= 0;
+	// private Display myDisplay = null;
+	private int						frameNumber			= 0;
+	private long					expiration			= 0;
+	private long					lastAdvanceTime		= 0;
 
 	/**
 	 * Create a channel list to play out on the Display.
@@ -44,16 +41,14 @@ public class ChannelPlayList implements Channel {
 	 * @param list
 	 *            - The list of channels to play (must be 2 or greater)
 	 * @param millisecondDwellTime
-	 *            - How long to wait on each channel (must be 5 seconds or longer)
+	 *            - How long to wait on each channel (must be 2 seconds or longer)
 	 */
 	public ChannelPlayList(List<SignageContent> list, long millisecondDwellTime) {
 		assert (list.size() > 1);
-		assert (millisecondDwellTime > 5000);
+		assert (millisecondDwellTime > 2000);
 
 		this.dwell = millisecondDwellTime;
 		this.channels = list;
-		chanIterator = channels.listIterator();
-		currentChannel = chanIterator.next(); // Get to the first channel
 	}
 
 	/**
@@ -64,76 +59,76 @@ public class ChannelPlayList implements Channel {
 
 	@Override
 	public String getName() {
-		return currentChannel.getName() + " (List)";
+		return channels.get(currentChannel).getName() + " (List)";
 	}
 
-	protected void informDisplay() {
-		if (myDisplay != null) {
-			myDisplay.setContent(currentChannel);
-		}
-	}
+	// protected void informDisplay() {
+	// if (myDisplay != null) {
+	// myDisplay.setContent(currentChannel);
+	// }
+	// }
 
-	/**
-	 * @param d
-	 */
-	public void setDisplay(final Display d) {
-		myDisplay = d;
-	}
+	// /**
+	// * @param d
+	// */
+	// public void setDisplay(final Display d) {
+	// myDisplay = d;
+	// }
 
 	@Override
 	public String getDescription() {
-		return "List of channels, now showing " + currentChannel.getDescription();
+		return "List of channels, now showing " + channels.get(currentChannel).getDescription();
 	}
 
 	@Override
 	public ChannelCategory getCategory() {
-		return currentChannel.getCategory();
+		return channels.get(currentChannel).getCategory();
 	}
 
 	@Override
 	public SignageType getType() {
-		return currentChannel.getType();
+		return channels.get(currentChannel).getType();
 	}
 
 	@Override
 	public URI getURI() {
-		return currentChannel.getURI();
+		return channels.get(currentChannel).getURI();
 	}
 
 	@Override
 	public int getNumber() {
-		if (currentChannel instanceof Channel)
-			return ((Channel) currentChannel).getNumber();
+		if (channels.get(currentChannel) instanceof Channel)
+			return ((Channel) channels.get(currentChannel)).getNumber();
 		return 0;
 	}
 
 	/**
 	 * Start playing this list of Channels
 	 */
-	public void start() {
-		new Thread("PlayingChannelList") {
-			public void run() {
-				catchSleep(dwell);
-				while (running) {
-					if (chanIterator.hasNext()) {
-						currentChannel = chanIterator.next();
-					} else {
-						chanIterator = channels.listIterator();
-						currentChannel = chanIterator.next();
-					}
-					informDisplay();
-					catchSleep(dwell);
-				}
-			}
-		}.start();
-	}
+	// public void start() {
+	// new Thread("PlayingChannelList") {
+	// public void run() {
+	// catchSleep(dwell);
+	// while (running) {
+	// if (chanIterator.hasNext()) {
+	// currentChannel = chanIterator.next();
+	// } else {
+	// chanIterator = channels.listIterator();
+	// currentChannel = chanIterator.next();
+	// }
+	// informDisplay();
+	// catchSleep(dwell);
+	// }
+	// }
+	// }.start();
+	// }
 
 	/**
 	 * A polite request to stop playing the Channel list
 	 */
-	public void stop() {
-		running = false;
-	}
+	// public void stop() {
+	// running = false;
+	// }
 
 	@Override
 	public void setName(String name) {
@@ -172,14 +167,13 @@ public class ChannelPlayList implements Channel {
 	 */
 	public void setChannels(final List<SignageContent> channels) {
 		this.channels = channels;
-		chanIterator = this.channels.listIterator();
 	}
 
 	@Override
 	public long getTime() {
-		if (currentChannel.getTime() <= 0)
+		if (channels.get(currentChannel).getTime() <= 0)
 			return dwell;
-		return currentChannel.getTime();
+		return channels.get(currentChannel).getTime();
 	}
 
 	@Override
@@ -189,7 +183,7 @@ public class ChannelPlayList implements Channel {
 
 	@Override
 	public int getCode() {
-		return currentChannel.getCode();
+		return channels.get(currentChannel).getCode();
 	}
 
 	@Override
@@ -218,5 +212,27 @@ public class ChannelPlayList implements Channel {
 	@Override
 	public void setExpiration(long expire) {
 		this.expiration = expire;
+	}
+
+	/**
+	 * Point to the next channel in the list
+	 */
+	public void advanceChannel() {
+		if (lastAdvanceTime + channels.get(currentChannel).getTime() < System.currentTimeMillis()) {
+			// This channel is still valid, keep playing it.
+			return;
+		}
+		lastAdvanceTime = System.currentTimeMillis();
+		currentChannel++;
+		if (currentChannel >= channels.size())
+			currentChannel = 0;
+	}
+
+	@Override
+	public String toString() {
+		String retval = "List of length " + channels.size() + ": [" ;
+		for (SignageContent SC: channels) 
+			retval += "(" + SC.getName() + " @ " + SC.getTime() + "msec) ";
+		return retval;
 	}
 }
