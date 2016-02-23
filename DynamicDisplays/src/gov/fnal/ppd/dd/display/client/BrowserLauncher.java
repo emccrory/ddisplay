09a4cdd3b664d13b6f7c2c0c1be47d70d804a04e
Elliott McCrory <mccrory@fnal.gov>
2016-02-23 11:35:04 -0600

    Change the name of the plugin's log file so it is more easily recognizable

--- DynamicDisplays/src/gov/fnal/ppd/dd/display/client/BrowserLauncher.java
+++ DynamicDisplays/src/gov/fnal/ppd/dd/display/client/BrowserLauncher.java
@@ -110,9 +110,9 @@ public class BrowserLauncher {
 			try {
 				// extensions.remotecontrol.portNumber is the environment variable that gives the port number, I think (9/15/2014)
 				// I tried to set it (and several variations) using System.setProperty(), to no avail.
-				File file = new File(firefoxUser + ".log");
-				ProcessBuilder pb = new ProcessBuilder("firefox", "-new-instance", "-P", firefoxUser, "-remote-control"); 
-				//,"javascript:%20" + moveTo, url);
+				File file = new File("remotecontrol_plugin_messages_for_" + firefoxUser + ".log");
+				ProcessBuilder pb = new ProcessBuilder("firefox", "-new-instance", "-P", firefoxUser, "-remote-control");
+				// ,"javascript:%20" + moveTo, url);
 				pb.redirectErrorStream(true);
 				pb.redirectOutput(file);
 				List<String> c = pb.command();
