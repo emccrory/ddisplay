61e4fed20784d469d7dfba33727bad21f4dee774
Elliott McCrory <mccrory@fnal.gov>
2016-02-26 15:52:41 -0600

    Catch up with previous commits.

--- DynamicDisplays/runDisplayWindows.bat
+++ DynamicDisplays/runDisplayWindows.bat
@@ -1,4 +1,5 @@
+call config.cmd
 
+java -Xmx1024m gov.fnal.ppd.dd.display.client.DisplayAsConnectionToFireFox
 
-
-
+# Need to capture the output, and add more of the functionality from runDisplay.sh
