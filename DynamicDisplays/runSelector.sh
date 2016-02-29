147c817ecf036fc7070392ae40e3ea1909c4ff3a
Elliott McCrory <mccrory@fnal.gov>
2016-02-29 10:07:01 -0600

    hard-code the clients that need the "raise now" button" (This should be in the database, though.)

--- DynamicDisplays/runSelector.sh
+++ DynamicDisplays/runSelector.sh
@@ -39,7 +39,15 @@ fi
 
 d=`date +%F`
 
+MyName=`uname -n`
+raiseButton=false
+# TODO Remove this bit of hard coding.  Put it in the DB or something
+if [ $MyName = "xocnuc01.fnal.gov" -o $MyName = "minos-nuc-01.dhcp.fnal.gov" -o $MyName = "display-neutrino-01.fnal.gov" -o $MyName = "wh8w-nuc-01.fnal.gov" ]; then
+    raiseButton=true;
+fi
+
 java -Dddisplay.selector.inwindow=$window \
      -Dddisplay.selector.public=$public \
+     -Dddisplay.selector.showraisebutton=$raiseButton \
      -Dddisplay.virtualdisplaynumbers=TRUE \
      -Xmx1024m  gov.fnal.ppd.dd.MakeChannelSelector > ../../log/selector_${d}_$$.log 2>&1  &
