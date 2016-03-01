df20dc07a9beb5de12f34f64ac3b8ab8ab2e7a54
Elliott McCrory <mccrory@fnal.gov>
2016-03-01 12:23:05 -0600

    Add "version" and "flavor" to the class

--- DynamicDisplays/src/gov/fnal/ppd/dd/util/TranslateGITHashCodeToDate.java
+++ DynamicDisplays/src/gov/fnal/ppd/dd/util/TranslateGITHashCodeToDate.java
@@ -4,7 +4,6 @@ import static gov.fnal.ppd.dd.GlobalVariables.DATABASE_NAME;
 import static gov.fnal.ppd.dd.GlobalVariables.credentialsSetup;
 import gov.fnal.ppd.dd.changer.ConnectionToDynamicDisplaysDatabase;
 
-import java.math.BigInteger;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -18,8 +17,10 @@ import java.util.Date;
  * 
  */
 public class TranslateGITHashCodeToDate {
-	private String	hashCode;
-	private Date	timeStamp;
+	private String	hashCode	= null;
+	private Date	timeStamp	= null;
+	private String	version		= null;
+	private String	flavor		= null;
 
 	/**
 	 * Simply return the newest hash code and time stamp
@@ -62,36 +63,48 @@ public class TranslateGITHashCodeToDate {
 	 *             -- If the provided hash is not in the DB.
 	 */
 	public Date getTimeStamp() throws Exception {
-		if (timeStamp != null)
-			return timeStamp;
-
-		// Look it up in the database
-
-		Connection connection = ConnectionToDynamicDisplaysDatabase.getDbConnection();
-		String query = "Select HashDate from GitHashDecode where HashCode='" + hashCode + "'";
-
-		synchronized (connection) {
-			try (Statement stmt = connection.createStatement(); ResultSet rs1 = stmt.executeQuery("USE " + DATABASE_NAME)) {
-				try (ResultSet rs = stmt.executeQuery(query)) {
-					if (rs.first())
-						do {
-							timeStamp = rs.getTimestamp("HashDate");
-						} while (rs.next());
-					else {
-						// Oops. no first element!?
-						throw new Exception("No elements in the GITHash table!");
-					}
-				} catch (SQLException e) {
-					System.err.println(query);
-					e.printStackTrace();
-				}
-			} catch (SQLException e) {
-				System.err.println(query);
-				e.printStackTrace();
-			}
-		}
+		getVersioning();
 
 		return timeStamp;
+
+	}
+
+	private void getVersioning() throws Exception {
+		if (timeStamp == null || version == null || flavor == null) {
+			// Look it up in the database
+
+			try {
+				Connection connection = ConnectionToDynamicDisplaysDatabase.getDbConnection();
+
+				String query = "Select HashDate,Version,Flavor from GitHashDecode where HashCode='" + hashCode + "'";
+
+				synchronized (connection) {
+					try (Statement stmt = connection.createStatement(); ResultSet rs1 = stmt.executeQuery("USE " + DATABASE_NAME)) {
+						try (ResultSet rs = stmt.executeQuery(query)) {
+							if (rs.first())
+								do {
+									timeStamp = rs.getTimestamp("HashDate");
+									version = rs.getString("Version");
+									flavor = rs.getString("Flavor");
+								} while (rs.next());
+							else {
+								// Oops. no first element!?
+								throw new Exception("No elements in the GITHash table!");
+							}
+						} catch (SQLException e) {
+							System.err.println(query);
+							e.printStackTrace();
+						}
+					} catch (SQLException e) {
+						System.err.println(query);
+						e.printStackTrace();
+					}
+				}
+			} catch (DatabaseNotVisibleException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+		}
 	}
 
 	public void getNewest() throws Exception {
@@ -132,6 +145,36 @@ public class TranslateGITHashCodeToDate {
 		this.timeStamp = timeStamp;
 	}
 
+	/**
+	 * @return the version
+	 */
+	public String getVersion() {
+		return version;
+	}
+
+	/**
+	 * @param version
+	 *            the version to set
+	 */
+	public void setVersion(String version) {
+		this.version = version;
+	}
+
+	/**
+	 * @return the flavor
+	 */
+	public String getFlavor() {
+		return flavor;
+	}
+
+	/**
+	 * @param flavor
+	 *            the flavor to set
+	 */
+	public void setFlavor(String flavor) {
+		this.flavor = flavor;
+	}
+
 	public static void main(String[] args) {
 		boolean succinct = true;
 
@@ -143,8 +186,9 @@ public class TranslateGITHashCodeToDate {
 				if (succinct) {
 					System.out.println(hashcodeTranslation.getHashCode() + "\t" + hashcodeTranslation.getTimeStamp());
 				} else {
-					System.out.println("The newest time stamp is " + hashcodeTranslation.getHashCode()
-							+ ".  the hash code for this time is " + hashcodeTranslation.getTimeStamp());
+					System.out.println("For the hash code " + hashcodeTranslation.getHashCode()
+							+ ".\n\tTime stamp " + hashcodeTranslation.getTimeStamp() + ".\n\tVersion number "
+							+ hashcodeTranslation.getVersion() + ".\n\tFlavor: " + hashcodeTranslation.getFlavor());
 
 				}
 				return;
@@ -157,10 +201,11 @@ public class TranslateGITHashCodeToDate {
 				try {
 					hashcodeTranslation.setHashCode(ARG);
 					if (succinct)
-						System.out.println(hashcodeTranslation.getTimeStamp());
+						System.out.println(hashcodeTranslation.getTimeStamp() + ", Version " + hashcodeTranslation.getVersion());
 					else
 						System.out.println("Hash Code of '" + hashcodeTranslation.getHashCode() + "' has a timestamp of "
-								+ hashcodeTranslation.getTimeStamp());
+								+ hashcodeTranslation.getTimeStamp() + "; " + hashcodeTranslation.getFlavor() + ", "
+								+ hashcodeTranslation.getVersion());
 				} catch (Exception e) {
 					if (succinct)
 						System.out.println("(    No Time Stamp  )");
