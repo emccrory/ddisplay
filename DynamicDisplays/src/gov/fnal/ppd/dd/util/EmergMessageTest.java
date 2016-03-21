08a7ccc01ced21024b5022a82774619b369e53be
Elliott McCrory <mccrory@fnal.gov>
2016-03-21 14:08:48 -0500

    Test implementation of emergency communication

---
+++ DynamicDisplays/src/gov/fnal/ppd/dd/util/EmergMessageTest.java
@@ -0,0 +1,139 @@
+package gov.fnal.ppd.dd.util;
+
+import java.net.URI;
+
+import gov.fnal.ppd.dd.changer.ChannelCategory;
+import gov.fnal.ppd.dd.signage.EmergencyCommunication;
+import gov.fnal.ppd.dd.signage.SignageType;
+
+/**
+ * Reference implementation of EmergencyCommunication
+ * 
+ * @author Elliott McCrory, Fermilab AD/Instrumentation
+ * 
+ */
+public class EmergMessageTest implements EmergencyCommunication {
+
+	private static final long	serialVersionUID	= 4875847440423308800L;
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setName(String name) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public String getDescription() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setDescription(String d) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public ChannelCategory getCategory() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setCategory(ChannelCategory c) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public SignageType getType() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setType(SignageType t) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public URI getURI() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setURI(URI i) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public long getTime() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void setTime(long time) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public long getExpiration() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void setExpiration(long expire) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public int getCode() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void setCode(int n) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public int getFrameNumber() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void setFrameNumber(int frameNumber) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public String getMessage() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setMessage(String theMessage) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
