18162337a88e0d467493f45b9a8548e19451c7e2
Elliott McCrory <mccrory@fnal.gov>
2016-04-18 12:55:28 -0500

    Test class for touch-scrolling.

---
+++ DynamicDisplays/src/gov/fnal/ppd/dd/testing/DraggablePanel.java
@@ -0,0 +1,19 @@
+package gov.fnal.ppd.dd.testing;
+
+/**
+ * 
+ * Copied from the Internet, this would be an example of code, using JavaFX, that allows for gestured manipulation of a Java GUI
+ * panel, e.g., touch scrolling.
+ * 
+ * @author Elliott McCrory, Fermilab AD/Instrumentation
+ * 
+ */
+public final class DraggablePanel {
+
+	// The source has been moved to DraggablePanel_DOT_java.txt so as to make this unit compilable.
+
+	// When we have JavaFX available, we can try it again.
+
+	// E. McCrory, 4/18/2016
+
+}
