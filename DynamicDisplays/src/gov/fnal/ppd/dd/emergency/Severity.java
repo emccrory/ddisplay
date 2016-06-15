da98253113ddc973977a63d9d2ddba55b0ed59a3
Elliott McCrory <mccrory@fnal.gov>
2016-06-15 11:04:49 -0500

    Comment changes

--- DynamicDisplays/src/gov/fnal/ppd/dd/emergency/Severity.java
+++ DynamicDisplays/src/gov/fnal/ppd/dd/emergency/Severity.java
@@ -1,24 +1,31 @@
 package gov.fnal.ppd.dd.emergency;
 
+/**
+ * The Severity is use to distinguish among the sorts of messages that could be displayed. In particular, one might imagine that the
+ * messages become bolder and more prominent ans they get more severe.
+ * 
+ * @author Elliott McCrory, Fermilab AD/Instrumentation
+ * 
+ */
 public enum Severity {
-	
+
+	/**
+	 * The most severe sort of message
+	 */
+	EMERGENCY,
+
 	/**
 	 * 
 	 */
-	EMERGENCY, 
-	
+	ALERT,
+
 	/**
 	 * 
 	 */
-	ALERT, 
-	
+	INFORMATION,
+
 	/**
-	 * 
-	 */
-	INFORMATION, 
-	
-	/**
-	 * 
+	 * The least severe sort of message (in fact, not an emergency at all)
 	 */
 	TESTING,
 
