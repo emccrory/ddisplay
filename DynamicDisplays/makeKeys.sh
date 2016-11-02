d94dc9dfaa0a5228f200ea9c3a6827aae585df54
Elliott McCrory <mccrory@fnal.gov>
2016-11-02 10:40:47 -0500

    Make a shell script to help the Linux folks create the public/private key pair.

--- DynamicDisplays/makeKeys.sh
+++ DynamicDisplays/makeKeys.sh
@@ -8,5 +8,5 @@ CLIENTNAME=`hostname | sed 's/.fnal.gov//g'`
 java -Dddisplay.dbserver=fnalmysqldev.fnal.gov:3311 -Dddisplay.dbname=$DBNAME \
      gov.fnal.ppd.security.GenerateNewKeyPair Public.key private$CLIENTNAME\ selector\ 00.key $CLIENTNAME\ selector\ 00 $DBUSER
 
-mv Public.key \"$private\" ~/keystore
+mv *.key ~/keystore
 # ls -l ~/keystore
