999303804a1350d097b4e924580822342bc0620d
Elliott McCrory <mccrory@fnal.gov>
2017-06-20 15:29:36 -0500

    New shell and batch scripts for running the ChannelListGUI

---
+++ DynamicDisplays/runChannelListGUI.bat
@@ -0,0 +1,5 @@
+call config.cmd
+
+java -Dddisplay.selector.inwindow=FALSE ^
+     -Xmx1024m gov.fnal.ppd.dd.channel.list.ChannelListGUI
+
---
+++ DynamicDisplays/runChannelListGUI.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+cd ~/src/roc-dynamicdisplays/DynamicDisplays
+
+./runVersionInformation.sh
+
+. setupJars.sh
+
+d=`date +%F`
+
+java -Dddisplay.selector.inwindow=false \
+     -Xmx1024m  gov.fnal.ppd.dd.channel.list.ChannelListGUI > ../../log/channelListGUI_${d}_$$.log 2>&1  &
