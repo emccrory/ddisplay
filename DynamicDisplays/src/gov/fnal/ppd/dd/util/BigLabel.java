13406c5f637746b97ade9a69f760870a44c2089b
Elliott McCrory <mccrory@fnal.gov>
2018-04-03 13:02:19 -0500

    Move to here

---
+++ DynamicDisplays/src/gov/fnal/ppd/dd/util/BigLabel.java
@@ -0,0 +1,35 @@
+package gov.fnal.ppd.dd.util;
+
+import static gov.fnal.ppd.dd.GlobalVariables.SHOW_IN_WINDOW;
+
+import java.awt.Color;
+import java.awt.Font;
+
+import javax.swing.JComponent;
+import javax.swing.JLabel;
+
+/**
+ * A utility class used in more than one place.
+ * 
+ * @author Elliott McCrory, Fermilab AD/Instrumentation
+ * @copyright 2017
+ * 
+ */
+public class BigLabel extends JLabel {
+	private static final long	serialVersionUID	= 8296427549410976741L;
+
+	/**
+	 * @param title
+	 *            The text on the label
+	 * @param style
+	 *            the style (BOLD, PLAIN) of the label
+	 */
+	public BigLabel(final String title, final int style) {
+		super(title);
+		setOpaque(true);
+		setBackground(Color.white);
+
+		setFont(new Font("Arial", style, (SHOW_IN_WINDOW ? 16 : 24)));
+		setAlignmentX(JComponent.CENTER_ALIGNMENT);
+	}
+}
