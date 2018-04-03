ba392dec4c917b9965249f5b7b1468a86b6ccb61
Elliott McCrory <mccrory@fnal.gov>
2018-04-03 12:52:15 -0500

    Make sure there is at least one row

--- DynamicDisplays/src/gov/fnal/ppd/dd/changer/SaveRestoreDefaultChannels.java
+++ DynamicDisplays/src/gov/fnal/ppd/dd/changer/SaveRestoreDefaultChannels.java
@@ -54,7 +54,6 @@ import javax.swing.BorderFactory;
 import javax.swing.Box;
 import javax.swing.JButton;
 import javax.swing.JComponent;
-import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
