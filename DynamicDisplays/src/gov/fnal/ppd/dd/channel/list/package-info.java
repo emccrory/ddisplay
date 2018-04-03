174c5675911cfaaac2ecc60d11ea1f62ba1d998a
Elliott McCrory <mccrory@fnal.gov>
2018-04-03 13:00:29 -0500

    Comment changes

--- DynamicDisplays/src/gov/fnal/ppd/dd/channel/list/package-info.java
+++ DynamicDisplays/src/gov/fnal/ppd/dd/channel/list/package-info.java
@@ -1,7 +1,7 @@
 /**
  * <p>Fermilab's Dynamic Displays System -- The classes that implement the channel list construction GUI for the ChannelSelector</p>
  * 
- * <p>These classes are "version 2" of the GUI -- they are based on the JTable class</p>
+ * <p>These classes are "version 2" of the Channel List GUI's -- they are based on the JTable class</p>
  * 
  */
 package gov.fnal.ppd.dd.channel.list;
