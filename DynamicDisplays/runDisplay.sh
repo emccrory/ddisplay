f0de24b6439ff7ca8a6b8e1d6c8fc53bb9fe2eb7
Elliott McCrory <mccrory@fnal.gov>
2018-05-29 13:07:56 -0500

    Remove the firefox config - class files are absent (by and large)

--- DynamicDisplays/runDisplay.sh
+++ DynamicDisplays/runDisplay.sh
@@ -130,7 +130,7 @@ fi
 	lsof -i :49999
 
         # Do we need to run two instances of the display?  Then fix the position of the firefox windows
-	./fixFirefoxConfig.sh
+	# ./fixFirefoxConfig.sh -- not working on all display nodes.
 
 	java -Xmx1024m gov.fnal.ppd.dd.display.client.DisplayAsConnectionToFireFox 
 
