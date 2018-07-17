904e5de7e6a15a96f02d996f3dbce2b28722f83b
Elliott McCrory <mccrory@fnal.gov>
2018-07-17 08:50:31 -0500

    Granularize the error messages a little

--- DynamicDisplays/src/gov/fnal/ppd/dd/display/client/selenium/CheckAndFixScreenDimensions.java
+++ DynamicDisplays/src/gov/fnal/ppd/dd/display/client/selenium/CheckAndFixScreenDimensions.java
@@ -11,30 +11,31 @@ import org.openqa.selenium.Dimension;
 import org.openqa.selenium.Point;
 import org.openqa.selenium.WebDriver;
 
+import gov.fnal.ppd.dd.util.PropertiesFile;
+
 /**
  * A thread to keep an eye on the screen size. In principle, this will fix to back to full screen when it varies.
  * 
- * First attempt does not work because of an internal exception generated during the call to driver.manage().window().getPosition() -
- *   <date> org.openqa.selenium.remote.ErrorCodes toStatus
- *   INFO: HTTP Status: '404' -> incorrect JSON status mapping for 'unknown error' (500 expected)
- *   [...]
- *   at org.openqa.selenium.remote.RemoteWebDriver$RemoteWebDriverOptions$RemoteWindow.getPosition(RemoteWebDriver.java:817)
- *   [...]
- *   (And Line 817 is not a valid line number in the library, somehow.)
+ * First attempt does not work because of an internal exception generated during the call to driver.manage().window().getPosition()
+ * - <date> org.openqa.selenium.remote.ErrorCodes toStatus INFO: HTTP Status: '404' -> incorrect JSON status mapping for 'unknown
+ * error' (500 expected) [...] at
+ * org.openqa.selenium.remote.RemoteWebDriver$RemoteWebDriverOptions$RemoteWindow.getPosition(RemoteWebDriver.java:817) [...] (And
+ * Line 817 is not a valid line number in the library, somehow.)
  * 
  * @author Elliott McCrory, Fermilab AD/Instrumentation
  *
  */
 public class CheckAndFixScreenDimensions extends TimerTask {
 
-	private static final boolean	directPositioning	= false;
-	private WebDriver				driver;
-	private Dimension				screenDimension;
-	private Point					screenPosition;
+	private final boolean	directPositioning;
+	private final WebDriver	driver;
+	private final Dimension	screenDimension;
+	private final Point		screenPosition;
 
-	private int						counter				= 0;
-	private boolean					haventShowTraceback	= true;
-	private int						numExceptions		= 0;
+	private int				counter					= 0;
+	private boolean			haventShowTraceback1	= true;
+	private boolean			haventShowTraceback2	= true;
+	private int				numExceptions			= 0;
 
 	/**
 	 * @param d
@@ -51,17 +52,18 @@ public class CheckAndFixScreenDimensions extends TimerTask {
 		driver = d;
 		screenDimension = sd;
 		screenPosition = p;
+		directPositioning = PropertiesFile.getProperty("directPositioning").equalsIgnoreCase("true");
 	}
 
 	public void run() {
+		boolean adjusted = false;
 		try {
 			Point pos = driver.manage().window().getPosition();
 			Dimension dim = driver.manage().window().getSize();
-			boolean adjusted = false;
 
 			if (!dim.equals(screenDimension)) {
 				// The size has changed!
-				printlnErr(getClass(), " Screen dimensions have changed from " + screenDimension + " to " + dim);
+				printlnErr(getClass(), "Screen dimensions have changed from " + screenDimension + " to " + dim);
 				adjusted = true;
 			}
 			if (!pos.equals(screenPosition)) {
@@ -69,28 +71,35 @@ public class CheckAndFixScreenDimensions extends TimerTask {
 				printlnErr(getClass(), "Screen position have changed from " + screenPosition + " to " + pos);
 				adjusted = true;
 			}
-
+		} catch (Exception e) {
+			numExceptions++;
+			printlnErr(getClass(),
+					"Caught exception (#" + numExceptions + "/" + counter + ") in trying to read the position/size of the screen");
+			if (haventShowTraceback1) {
+				e.printStackTrace();
+				haventShowTraceback1 = false;
+			}
+		}
+		try {
 			if (adjusted) {
+				catchSleep(100);
 				if (directPositioning) {
 					driver.manage().window().setPosition(screenPosition);
 					driver.manage().window().setSize(screenDimension);
 				} else {
 					driver.findElement(new By.ByName("hiddenButton")).click();
 				}
-			}
-
-			if (adjusted || counter < 25 || counter % 25 == 0) {
 				catchSleep(100);
-				println(getClass(), counter + " Window layout - Popsition: " + driver.manage().window().getPosition()
+				println(getClass(), counter + " Window layout - Position: " + driver.manage().window().getPosition()
 						+ ", Dimensions: " + driver.manage().window().getSize());
 			}
 		} catch (Exception e) {
-			if (numExceptions++ % 30 == 0)
-				printlnErr(getClass(),
-						"Caught exception (" + numExceptions + ") in trying to read or adjust the position of the screen");
-			if (haventShowTraceback) {
+			numExceptions++;
+			printlnErr(getClass(),
+					"Caught exception (#" + numExceptions + "/" + counter + ") in trying to adjust the position of the screen");
+			if (haventShowTraceback2) {
 				e.printStackTrace();
-				haventShowTraceback = false;
+				haventShowTraceback2 = false;
 			}
 		}
 		++counter;
