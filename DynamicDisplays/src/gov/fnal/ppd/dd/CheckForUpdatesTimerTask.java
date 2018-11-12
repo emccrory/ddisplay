7bd154abbf8a4fc07e869761e76e70dffbde13a1
Elliott McCcrory <mccrory@fnal.gov>
2018-11-12 11:00:26 -0600

    Reformatting and cleaning up the GUI a little

--- DynamicDisplays/src/gov/fnal/ppd/dd/CheckForUpdatesTimerTask.java
+++ DynamicDisplays/src/gov/fnal/ppd/dd/CheckForUpdatesTimerTask.java
@@ -13,6 +13,7 @@ import java.util.TimerTask;
 
 import javax.swing.BorderFactory;
 import javax.swing.Box;
+import javax.swing.JComponent;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
@@ -29,7 +30,7 @@ import gov.fnal.ppd.dd.util.version.VersionInformationComparison;
  *
  */
 public class CheckForUpdatesTimerTask extends TimerTask {
-	private JFrame	frame;
+	private JFrame frame;
 
 	public CheckForUpdatesTimerTask() {
 	}
@@ -41,7 +42,7 @@ public class CheckForUpdatesTimerTask extends TimerTask {
 			Calendar date = Calendar.getInstance();
 			int dow = date.get(Calendar.DAY_OF_WEEK);
 			int hour = date.get(Calendar.HOUR_OF_DAY);
-			if (dow == Calendar.SATURDAY || dow == Calendar.SUNDAY || (dow == Calendar.MONDAY && hour < 10) ) {
+			if (dow == Calendar.SATURDAY || dow == Calendar.SUNDAY || (dow == Calendar.MONDAY && hour < 10)) {
 				println(getClass(), "Skipping update check on weekends");
 				return;
 			}
@@ -53,7 +54,7 @@ public class CheckForUpdatesTimerTask extends TimerTask {
 
 			if (VersionInformationComparison.lookup(flavor, true)) {
 
-				// 2. If so, download it. 
+				// 2. If so, download it.
 				VersionInformation viWeb = VersionInformation.getDBVersionInformation(flavor);
 				showNewUpdateInformation(flavor, viWeb.getVersionString());
 
@@ -79,37 +80,47 @@ public class CheckForUpdatesTimerTask extends TimerTask {
 		}
 	}
 
+	private static class LocalJLabel extends JLabel {
+		private static final long serialVersionUID = 4744851697649452477L;
+
+		public LocalJLabel(String title, int style, int size) {
+			super(title);
+			setAlignmentX(JComponent.CENTER_ALIGNMENT);
+			setFont(new Font("Arial", style, size));
+		}
+	}
+
 	private void showNewUpdateInformation(FLAVOR flavor, String versionString) {
 		// Isolate the GUI stuff here.
-		String message1 = "There is a new " + flavor + " version of the software, Version " + versionString + ".";
-		String message2 = "This version is newer than the code we are running.";
-		String message3 = "Updating the software and then restarting this application program.";
-		println(getClass(), message1 + message2 + "\n" + message3);
+		String message0 = "Dynamic Displays Software Update in progress";
+		String message1 = "There is a new " + flavor + " version, " + versionString + ", of the Dynamic Displays software.";
+		String message3 = "After the software has been updated, the application program will be restarted.";
+		println(getClass(), message0 + message1 + "\n" + message3);
 
+		Dimension bigGap = new Dimension(60,60);
+		
 		frame = new JFrame("Dynamic Displays: New Software Update");
 		frame.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
 
-		JLabel big = new JLabel(message1);
-		big.setFont(new Font("Arial", Font.BOLD, 32));
 		Box p = Box.createVerticalBox();
-		p.add(big);
-		big = new JLabel(message2);
-		big.setFont(new Font("Arial", Font.BOLD, 32));
-		p.add(Box.createRigidArea(new Dimension(80, 80)));
+		JLabel big = new LocalJLabel(message0, Font.BOLD, 46);
 		p.add(big);
 
-		big = new JLabel(message3);
-		big.setFont(new Font("Arial", Font.ITALIC, 32));
-		p.add(Box.createRigidArea(new Dimension(80, 80)));
+		big = new LocalJLabel(message1, Font.PLAIN, 32);
+		p.add(Box.createRigidArea(bigGap));
 		p.add(big);
 
-		big = new JLabel("This will take about 5 minutes, " + new Date());
-		big.setFont(new Font("Arial", Font.ITALIC, 18));
-		p.add(Box.createRigidArea(new Dimension(80, 80)));
+		big = new LocalJLabel(message3, Font.ITALIC, 32);
+		p.add(Box.createRigidArea(bigGap));
+		p.add(big);
+
+		big = new LocalJLabel("This will take about 5 minutes, " + new Date(), Font.ITALIC, 18);
+		p.add(Box.createRigidArea(bigGap));
 		p.add(big);
 
 		p.setBorder(BorderFactory.createCompoundBorder(BorderFactory.createEmptyBorder(30, 30, 30, 30),
-				BorderFactory.createLineBorder(Color.red.darker(), 20)));
+				BorderFactory.createCompoundBorder(BorderFactory.createLineBorder(Color.red.darker(), 20),
+						BorderFactory.createEmptyBorder(10, 10, 10, 10))));
 
 		frame.setContentPane(p);
 		frame.pack();
