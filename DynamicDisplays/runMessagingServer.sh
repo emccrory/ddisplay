435502c2481a024dd8b595b0a3c03aa606abc9e7
Elliott McCcrory <mccrory@fnal.gov>
2019-02-04 10:59:30 -0600

    Change the name of the log file to, simply, messagingServer.log.  Make appropriate adjustments so this file is saved the next time.

--- DynamicDisplays/runMessagingServer.sh
+++ DynamicDisplays/runMessagingServer.sh
@@ -6,13 +6,23 @@ cd ~/src/roc-dynamicdisplays/DynamicDisplays
 
 d=`date +%F`
 
+cd ../../log
+
+if [ -e messagingServer.log ]; then
+    # Rename the existing messagingServer.log with time stamp of the first access (creation time)
+    suffix=`stat messagingServer.log | grep "Access: 2" | cut -b 9-27 | sed 's/ /_/g' | sed 's/:/./g'`
+    # This command pipe Assumes A LOT!  So it will probably be brittle
+
+    mv messagingServer.log messagingServer_$suffix.log
+fi
+
+# This compression might take some time, so push it into the background
+gzip messagingServer_*.log &
+
+cd ../roc-dynamicdisplays/DynamicDisplays
+
 {
     /bin/date
-    java -Xmx1024m \
-	-Dddisplay.dbserver=$databaseServer \
-	-Dddisplay.dbname=$databaseName \
-	-Dddisplay.dbusername=$databaseUsername \
-	-Dddisplay.dbpassword=$databasePassword \
-	gov.fnal.ppd.dd.chat.MessagingServerGUI
-} > ../../log/messagingServer_${d}_$$.log 2>&1
+    java -Xmx1024m gov.fnal.ppd.dd.chat.MessagingServerGUI
+} > ../../log/messagingServer.log 2>&1
 
