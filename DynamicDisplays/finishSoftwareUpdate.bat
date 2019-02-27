41aff1b48c5d8dfffe29a5695560efd7ab8ef302
Elliott McCcrory <mccrory@fnal.gov>
2019-02-27 11:33:02 -0600

    Look for updated software folder.  This will be the script that is run for the Windows-based selector nodes.

--- DynamicDisplays/finishSoftwareUpdate.bat
+++ DynamicDisplays/finishSoftwareUpdate.bat
@@ -4,37 +4,46 @@ REM Unpack a new version of the software on a Windows PC - Part 3
 
 REM The working directory should be ..\.. with respect to where the software usually runs.
 
-echo %cd%
+ECHO OFF
+ECHO %cd%
 
-SET A=1
-:nextpart
-SET file=roc-dynamicdisplays-old00%A%
-if %A% GTR 9 (
-	SET file=roc-dynamicdisplays-old0%A%
+IF EXISTS roc-dynamicdisplays-new (
+  ECHO A new version of the Dynamic Displays software has been downloaded
+  SET A=1
+  :nextpart
+  SET file=roc-dynamicdisplays-old00%A%
+  if %A% GTR 9 (
+	  SET file=roc-dynamicdisplays-old0%A%
+  )
+  if %A% GTR 99 (
+	  SET file=roc-dynamicdisplays-old%A%
+  )
+  if %A% GTR 999 (
+	  ECHO Too many folders here
+	  GOTO foundit
+  )
+  if not exist %file% GOTO foundit
+  SET /a A=%A%+1
+  GOTO nextpart
+
+  :foundit
+  ECHO Downloaded the new version and now renaming roc-dynamicdisplays folder to %file%
+
+  ECHO ON
+ 
+  RENAME roc-dynamicdisplays %file%
+  ECHO OFF
+
+  ECHO Now renaming roc-dynamicdisplays-new folder to roc-dynamicdisplays
+  ECHO ON
+  RENAME roc-dynamicdisplays-new roc-dynamicdisplays
+  ECHO OFF
+
+  ECHO All done with the update
+) else (
+  ECHO There is no update available at this time
 )
-if %A% GTR 99 (
-	SET file=roc-dynamicdisplays-old%A%
-)
-if %A% GTR 999 (
-	ECHO Too many folders here
-	GOTO foundit
-)
-if not exist %file% GOTO foundit
-SET /a A=%A%+1
-GOTO nextpart
-
-:foundit
-ECHO Downloaded the new version and now renaming roc-dynamicdisplays folder to %file%
-
-ECHO ON
-
-RENAME roc-dynamicdisplays %file%
-
-ECHO Now renaming roc-dynamicdisplays-new folder to roc-dynamicdisplays
-RENAME roc-dynamicdisplays-new roc-dynamicdisplays
-
-ECHO All done with the update
-
+  
 REM Now we need to figure out what program to run to restart the system.  At this time, all Windows
 REM nodes only run the channel selector.  So lets go with that.
 
