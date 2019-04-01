f21df19406a6094abf379f5ce7aee4ce6f0e1981
Elliott McCcrory <mccrory@fnal.gov>
2019-04-01 14:44:51 -0500

    This class was removed, but it has been realized that the database has traces of this class in, for example, the location save mechanism.  So, we put the class back for backwards compatibility (but mark it as deprecated).

---
+++ DynamicDisplays/src/gov/fnal/ppd/dd/xml/EmptyChannel.java
@@ -0,0 +1,43 @@
+package gov.fnal.ppd.dd.xml;
+
+import static gov.fnal.ppd.dd.util.Util.MY_NAME;
+import static gov.fnal.ppd.dd.util.Util.MY_URL;
+import gov.fnal.ppd.dd.changer.ChannelCategory;
+import gov.fnal.ppd.dd.channel.ChannelImpl;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+/**
+ * An empty channel for use by these XML classes - This used to be the name of the class, but it was changed to XmlDerivedChannel.
+ * This class has been reintroduced so that channels stored in the table DefaultChannels will still work.
+ * 
+ * @author Elliott McCrory, Fermilab/AD/Instrumentation.
+ * 
+ * @deprecated
+ */
+public class EmptyChannel extends ChannelImpl {
+	private static final long serialVersionUID = -419375552528263824L;
+
+	/**
+	 * Create an Empty Channel
+	 * 
+	 * @throws URISyntaxException
+	 *             (In the unlikely case that the default web page is no longer a valid URI)
+	 */
+	public EmptyChannel() throws URISyntaxException {
+		this(MY_NAME, new ChannelCategory("PUBLIC", "PUBLIC"));
+
+	}
+
+	public EmptyChannel(String string, ChannelCategory categ) throws URISyntaxException {
+		super(string, categ, "This channel is " + string, new URI(MY_URL), 0, 0);
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(final String[] args) {
+		System.out.println(MY_URL);
+	}
+}
