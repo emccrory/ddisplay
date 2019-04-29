9c48cc1b02354fad62c1cadad96fc852fcd408fa
Elliott McCcrory <mccrory@fnal.gov>
2019-04-29 15:31:12 -0500

    Add hashCode() and equals() methods

--- DynamicDisplays/src/gov/fnal/ppd/dd/xml/ChangeChannel.java
+++ DynamicDisplays/src/gov/fnal/ppd/dd/xml/ChangeChannel.java
@@ -52,7 +52,37 @@ public class ChangeChannel extends EncodedCarrier {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((content == null) ? 0 : content.hashCode());
+		result = prime * result + displayNum;
+		result = prime * result + screenNum;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ChangeChannel other = (ChangeChannel) obj;
+		if (content == null) {
+			if (other.content != null)
+				return false;
+		} else if (!content.equals(other.content))
+			return false;
+		if (displayNum != other.displayNum)
+			return false;
+		if (screenNum != other.screenNum)
+			return false;
+		return true;
 	}
 
 }
--- DynamicDisplays/src/gov/fnal/ppd/dd/xml/ChannelSpec.java
+++ DynamicDisplays/src/gov/fnal/ppd/dd/xml/ChannelSpec.java
@@ -141,4 +141,31 @@ public class ChannelSpec implements SignageContent {
 	public void setURI(URI i) {
 		content.setURI(i);
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((content == null) ? 0 : content.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ChannelSpec other = (ChannelSpec) obj;
+		if (content == null) {
+			if (other.content != null)
+				return false;
+		} else if (!content.equals(other.content))
+			return false;
+		return true;
+	}
+	
+	
 }
