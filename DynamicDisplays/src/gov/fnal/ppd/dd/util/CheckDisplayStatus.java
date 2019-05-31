93285ac195cb71332040155550fee6e5493da505
Elliott McCcrory <mccrory@fnal.gov>
2019-05-31 15:54:54 -0500

    Catch up with the rest of the suite: The statuses have the Content in XML; this was assuming the old java serialization mechanism

--- DynamicDisplays/src/gov/fnal/ppd/dd/util/CheckDisplayStatus.java
+++ DynamicDisplays/src/gov/fnal/ppd/dd/util/CheckDisplayStatus.java
@@ -21,16 +21,22 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.swing.JLabel;
 
 import gov.fnal.ppd.dd.changer.ChannelButtonGrid;
 import gov.fnal.ppd.dd.changer.DisplayButtons;
+import gov.fnal.ppd.dd.channel.ChannelPlayList;
+import gov.fnal.ppd.dd.chat.ErrorProcessingMessage;
 import gov.fnal.ppd.dd.db.ConnectionToDatabase;
 import gov.fnal.ppd.dd.display.DisplayFacade;
 import gov.fnal.ppd.dd.signage.Display;
 import gov.fnal.ppd.dd.signage.SignageContent;
+import gov.fnal.ppd.dd.xml.ChangeChannelList;
+import gov.fnal.ppd.dd.xml.ChannelSpec;
+import gov.fnal.ppd.dd.xml.MyXMLMarshaller;
 
 /**
  * Utility class for the main Channel Selector class.
@@ -101,33 +107,43 @@ public class CheckDisplayStatus extends Thread {
 					counter++;
 					// Use ARM (Automatic Resource Management) to assure that things get closed properly (a new Java 7
 					// feature)
+					String rawMessage = null;
 					try (Statement stmt = connection.createStatement(); ResultSet rs = stmt.executeQuery(query);) {
 						if (rs.first()) { // Move to first returned row
 							while (!rs.isAfterLast()) {
 								try {
-									Blob sc = rs.getBlob("SignageContent");
-									// TODO - The content of this Blob needs to be XML, not streamed objects
 									String contentName = rs.getString("Content");
+									Blob sc = rs.getBlob("SignageContent");
+									rawMessage = new String(sc.getBytes(1, (int) sc.length()));
 									SignageContent currentContent = null;
-
-									if (sc != null && sc.length() > 0) {
-										int len = (int) sc.length();
-										byte[] bytes = sc.getBytes(1, len);
-
-										ByteArrayInputStream fin = new ByteArrayInputStream(bytes);
-										ObjectInputStream ois = new ObjectInputStream(fin);
-										currentContent = (SignageContent) ois.readObject();
-										getContentOnDisplays().put(display, currentContent);
+									if (rawMessage.contains("changeChannelList")) {
+										ChangeChannelList ccl = (ChangeChannelList) MyXMLMarshaller
+												.unmarshall(ChangeChannelList.class, rawMessage);
+										List<SignageContent> lsc = new ArrayList<SignageContent>();
+										for (ChannelSpec C : ccl.getChannelSpec()) {
+											lsc.add(C);
+										}
+										currentContent = new ChannelPlayList(contentName, lsc, 1000000L);
+									} else if (rawMessage.contains("channelSpec")) {
+										currentContent = (ChannelSpec) MyXMLMarshaller.unmarshall(ChannelSpec.class, rawMessage);
+									} else {
+										throw new ErrorProcessingMessage(
+												"Unknown XML data type within this XML document: [Unrecognized XML message received] -- "
+														+ rawMessage);
 									}
-
 									if (display instanceof DisplayFacade && currentContent != null) {
 										if (!contentName.equals(SELF_IDENTIFY)) {
 											// It should fall here; all displays on this side are DisplayFacade's
 											DisplayFacade myDisplay = (DisplayFacade) display;
 											myDisplay.setContentValueBackdoor(currentContent);
-											// System.out.println("Status of Display " + display.getDBDisplayNumber() +
-											// " is being set to "
-											// + currentContent + "\t(" + currentContent.getClass().getSimpleName() + ")");
+											// println(getClass(), "Status of Display " + display.getDBDisplayNumber() + " is being
+											// set to " + currentContent + "\t(" + currentContent.getClass().getSimpleName()
+											// + ")");
+											Display o = getContentOnDisplays().get(myDisplay.getDBDisplayNumber());
+											if (o == null)
+												getContentOnDisplays().put(myDisplay, currentContent);
+											else
+												getContentOnDisplays().replace(o, currentContent);
 										}
 									}
 
@@ -178,16 +194,16 @@ public class CheckDisplayStatus extends Thread {
 													counter + " Trying to read streamed content object in database from display "
 															+ display.getDBDisplayNumber() + ", but it was corrputed: ["
 															+ e.getClass().getSimpleName() + " - " + e.getLocalizedMessage()
-															+ "]\n\tWill supress this specific error message for now.\n");
+															+ "]\n\tWill supress this specific error message for now.\n" + rawMessage);
 											lastStreamCorruptedMesage = e.getLocalizedMessage();
 										}
 									} else {
 										println(getClass(),
 												counter + " Trying to read streamed content object in database from display "
 														+ display.getDBDisplayNumber() + ", but it failed: ["
-														+ e.getClass().getSimpleName() + " - " + e.getLocalizedMessage() + "]");
+														+ e.getClass().getSimpleName() + " - " + e.getLocalizedMessage() + "]" + rawMessage);
 									}
-									// e.printStackTrace();
+									e.printStackTrace();
 								}
 								if (!rs.next())
 									break;
