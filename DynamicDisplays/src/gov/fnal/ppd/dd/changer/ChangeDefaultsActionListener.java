adf04fd0b9e9b0679ee5dcc7398be517833028f9
Elliott McCcrory <mccrory@fnal.gov>
2019-06-28 15:02:21 -0500

    Add a "dismiss" button to this popup window

--- DynamicDisplays/src/gov/fnal/ppd/dd/changer/ChangeDefaultsActionListener.java
+++ DynamicDisplays/src/gov/fnal/ppd/dd/changer/ChangeDefaultsActionListener.java
@@ -3,12 +3,16 @@ package gov.fnal.ppd.dd.changer;
 import static gov.fnal.ppd.dd.GlobalVariables.displayList;
 import static gov.fnal.ppd.dd.util.Util.println;
 
+import java.awt.Component;
 import java.awt.Container;
 import java.awt.Frame;
+import java.awt.Insets;
 import java.awt.Window;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.Box;
+import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.SwingUtilities;
 
@@ -33,7 +37,22 @@ public class ChangeDefaultsActionListener implements ActionListener {
 		
 		f1 = new JDialog(parentFrame, "Save / Restore Channels on all Displays", true);
 
-		f1.setContentPane(SaveRestoreDefaultChannels.getGUI());
+		Box all = Box.createVerticalBox();
+		all.add(SaveRestoreDefaultChannels.getGUI());
+		JButton button = new JButton("<html><i>Dismiss this window</i></html>");
+		button.setFont(button.getFont().deriveFont(24.0f));
+		button.setHorizontalAlignment(JButton.CENTER);
+		button.setAlignmentX(Component.CENTER_ALIGNMENT);
+		button.setMargin(new Insets(20, 50, 20, 50));
+		all.add(button);
+		button.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				f1.setVisible(false);				
+			}
+		});
+		f1.setContentPane(all);
 		int height = 400 + 30 * (displayList.size() + 1);
 		if (height > 1000)
 			height = 1000;
@@ -41,46 +60,5 @@ public class ChangeDefaultsActionListener implements ActionListener {
 		f1.setSize(900, height);
 		f1.setAlwaysOnTop(true);
 		f1.setVisible(true);
-		// f1.addWindowListener(new WindowListener() {
-		//
-		// @Override
-		// public void windowOpened(WindowEvent e) {
-		// }
-		//
-		// @Override
-		// public void windowIconified(WindowEvent e) {
-		// visible = false;
-		// }
-		//
-		// @Override
-		// public void windowDeiconified(WindowEvent e) {
-		// visible = true;
-		// }
-		//
-		// @Override
-		// public void windowDeactivated(WindowEvent e) {
-		// }
-		//
-		// @Override
-		// public void windowClosing(WindowEvent e) {
-		// }
-		//
-		// @Override
-		// public void windowClosed(WindowEvent e) {
-		// visible = false;
-		// f1.setVisible(false);
-		// }
-		//
-		// @Override
-		// public void windowActivated(WindowEvent e) {
-		// }
-		// });
-		// }
-		// visible = !visible;
-		// f1.setVisible(visible);
-		// if (visible) {
-		// f1.toFront();
-		// f1.repaint();
-		// }
 	}
 }
