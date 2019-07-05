5fe8e444a418aa515a7643f6879ae9806cd54412
Elliott McCcrory <mccrory@fnal.gov>
2019-07-05 16:39:11 -0500

    Remove un-needed FireFox plugins

--- FireFoxPlugins/FF-Remote-Control-master/defaults/preferences/prefs.js
+++
@@ -1,8 +0,0 @@
-pref("extensions.remotecontrol.localhostOnly", true);
-pref("extensions.remotecontrol.portNumber", 32000);
-pref("extensions.remotecontrol.activeTab", false);
-
-// https://developer.mozilla.org/en/Localizing_extension_descriptions
-pref("extensions.remote-control@morch.com.description", "chrome://remotecontrol/locale/overlay.properties");
-
-pref("extensions.remotecontrol.toolbarButtonInitiallyInstalled", false);
