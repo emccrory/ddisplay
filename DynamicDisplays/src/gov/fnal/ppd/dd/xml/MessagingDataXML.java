8a84173c58c26e903a4af557f32fb413e2409a82
Elliott McCcrory <mccrory@fnal.gov>
2019-10-07 15:00:22 -0500

    Use the new "Am Alive" class names

--- DynamicDisplays/src/gov/fnal/ppd/dd/xml/MessagingDataXML.java
+++ DynamicDisplays/src/gov/fnal/ppd/dd/xml/MessagingDataXML.java
@@ -2,9 +2,10 @@ package gov.fnal.ppd.dd.xml;
 
 import javax.xml.bind.annotation.XmlSeeAlso;
 
-@XmlSeeAlso(value = { ChangeChannel.class, ChangeChannelList.class, ChangeChannelByNumber.class, AmAliveMessage.class,
-		EmergencyMessXML.class, ErrorMessage.class, IsAliveMessage.class, LoginMessage.class, SubscriptionSubject.class,
+@XmlSeeAlso(value = { ChangeChannel.class, ChangeChannelList.class, ChangeChannelByNumber.class, YesIAmAliveMessage.class,
+		EmergencyMessXML.class, ErrorMessage.class, AreYouAliveMessage.class, LoginMessage.class, SubscriptionSubject.class,
 		WhoIsInMessage.class, WhoIsInReply.class })
+
 public class MessagingDataXML {
 
 	public MessagingDataXML() {
