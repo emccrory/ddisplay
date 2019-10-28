eabacc5e40aff44a5b50b7f2f2f6676d0220dd6b
Elliott McCcrory <mccrory@fnal.gov>
2019-10-28 14:19:18 -0500

    Add logic to remove the xterm windows that may be hanging around unnecessarily.

--- DynamicDisplays/runDisplay.sh
+++ DynamicDisplays/runDisplay.sh
@@ -62,6 +62,13 @@ fi >> $log 2>&1
 # kiosk mode enabled, won't let us get to the desktop
 cd $ddHome/log
 
+# Kill the exiting bash scripts that are in the src/log directory
+tempPIDs=temp$$
+pwdx `ps -aef | grep bash | grep -v grep | grep -v cron | awk '{ print $2 }'` 2>/dev/null | grep /log | awk '{ print $1 }' | sed 's/://g' > $tempPIDs
+if [ -s $tempPIDs ]; then
+    kill `cat $tempPIDs`
+fi
+
 if [ -e /usr/bin/xterm ]; then
     # SLF 6.x
     /usr/bin/xterm -geometry 200x30 -fa 'Monospace' -fs 12 &
