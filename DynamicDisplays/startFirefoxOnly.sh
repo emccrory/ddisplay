275312d472b472a7889143df411a6f146fd48bbf
Elliott McCcrory <mccrory@fnal.gov>
2019-11-14 16:30:01 -0600

    Change the main display script, runDisplay, to check if this is an instance of a "simplified" display.  if so, run the (new) Java class, DisplayAsSimpleBrowser.

--- DynamicDisplays/startFirefoxOnly.sh
+++ DynamicDisplays/startFirefoxOnly.sh
@@ -1,35 +1,15 @@
 #!/bin/bash
 
-# Start Firefox without Java and without Selenium.  This gives the user an electronic display, but
+# Start a java class that only starts Firefox, i.e., without Selenium.  This gives the user an electronic display, but
 #  1. It is not full screen (the user has to go in there and hit the F11 key)
-#  2. There is no control from a ChannelSelector
-#  3. The default channel cannot be a list of channels or a YouTube video (unless showing the video only once is OK)
+#  2. There is no emergency messaging display
+#  3. Some of the other "eye candy" on the display does not exist.
 
-# It may be appropriate to use this script for a lot of the displays since they almost never get changed. The
-# displays that use this could be marked in the database somehow. There are plenty of unused fields in the Display
-# table, for example, "Type" could be repurposed to say "Dynamic" or "Static", and "Static" would do this script.
+# It would be appropriate to use this script for a lot of the displays since they almost never get changed. 
 
-# The way that might work is to create a simple messaging client.  First it will use this scrupt to launch Firefox.  
-# Then it will listend for channel changes;  when it gets one, it will kill the Firefox process, and then 
-# re-launch Firefox again with the new URL.
+. setupJars.sh
 
-# But this method will not:
-#  -> Show lists of channels
-#  -> Show a YouTube video (YouTube does not let you do an "auto-repeat" directly)
+java gov.fnal.ppd.dd.display.simple.DisplayAsSimpleBrowser &
 
-# To be determined ...
-
-defaultURL=$1
-
-if [ "X $1" = "X " ]; then
-    . setupJars.sh
-
-    defaultURL=`java gov.fnal.ppd.dd.db.GetDefaultContentForDisplay | grep DEFAULTCONTENT | awk '{ print $2 }'`
-fi
-
-/usr/bin/firefox $defaultURL &
-
-echo $!
-
-# TODO - make Firefox go full screen (Kiosk mode).  It works rather wekll if you first start Firefox, 
+# TODO - make Firefox go full screen (Kiosk mode).  It works rather well if you first start Firefox, 
 # then hit F11, then exit Firefox - it remembers this state most of the time in Scientific Linux.
