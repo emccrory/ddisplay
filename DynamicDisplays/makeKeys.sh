6f1b568e17d2d7706e900c197ccf5f6065166e17
Elliott McCcrory <mccrory@fnal.gov>
2021-01-22 09:55:43 -0600

    Add class to tell the user what the JVM thinks their computer is named.  Fix up the makeKeys.sh script.

--- DynamicDisplays/makeKeys.sh
+++ DynamicDisplays/makeKeys.sh
@@ -5,8 +5,9 @@ DBUSER=insertpublickey
 DBNAME=xoc_prd
 CLIENTNAME=$(hostname | sed 's/.fnal.gov//g')
 
-java -Dddisplay.dbserver=vip-mariadb-prd.fnal.gov:3309 -Dddisplay.dbname=$DBNAME \
-     gov.fnal.ppd.security.GenerateNewKeyPair \""$CLIENTNAME"\\ selector\\ 00\" "$DBUSER"
+java gov.fnal.ppd.security.CheckHostName
+echo "Echoing the key commands here, rather than executing them..."
+echo java -Dddisplay.dbserver=vip-mariadb-prd.fnal.gov:3309 -Dddisplay.dbname=$DBNAME gov.fnal.ppd.security.GenerateNewKeyPair \""$CLIENTNAME"\\ selector\\ 00\" "$DBUSER"
 
-mv ./*.key ~/keystore
+echo mv ./*.key ~/keystore
 # ls -l ~/keystore
---
+++ DynamicDisplays/src/gov/fnal/ppd/security/CheckHostName.java
@@ -0,0 +1,19 @@
+package gov.fnal.ppd.security;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+public class CheckHostName {
+
+	public static void main(String[] args) {
+		String hostname = "localhost";
+		// Get the local hostname
+		try {
+			String h = InetAddress.getLocalHost().getCanonicalHostName();
+			hostname = h.substring(0, h.indexOf('.'));
+			System.out.println("The host name, according to the JVM, is '" + hostname + "' (derived from '" + h + "')");
+		} catch (UnknownHostException e1) {
+			e1.printStackTrace();
+		}		
+	}
+}
