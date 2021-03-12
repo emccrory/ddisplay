866b8b786a2ade20f718868bc13594b2f780ba92
Elliott McCcrory <mccrory@fnal.gov>
2021-03-12 12:51:21 -0600

    Bug fixes

--- DynamicDisplays/makeKeys.sh
+++ DynamicDisplays/makeKeys.sh
@@ -9,7 +9,7 @@ DBUSER=insertpublickey
 DBNAME=xoc_prd
 CLIENTNAME=$(hostname | sed 's/.fnal.gov//g' | sed 's/.local//g' )
 
-echo "Operating system name for this computer is " $CLIENTNAME
+echo "The system name from the operating system for this computer is " $CLIENTNAME
 java gov.fnal.ppd.security.CheckHostName
 echo java -Dddisplay.dbserver=vip-mariadb-prd.fnal.gov:3309 -Dddisplay.dbname=$DBNAME gov.fnal.ppd.security.GenerateNewKeyPair "$CLIENTNAME\\ selector\\ 00" "$DBUSER"
      java -Dddisplay.dbserver=vip-mariadb-prd.fnal.gov:3309 -Dddisplay.dbname=$DBNAME gov.fnal.ppd.security.GenerateNewKeyPair "$CLIENTNAME\\ selector\\ 00" "$DBUSER"
