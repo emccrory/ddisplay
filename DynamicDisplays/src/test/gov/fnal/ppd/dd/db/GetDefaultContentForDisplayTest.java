a0062a043bd07ae9cc973809079db2da8c74416d
Elliott McCcrory <mccrory@fnal.gov>
2021-04-22 09:50:45 -0500

    New test class

---
+++ DynamicDisplays/src/test/gov/fnal/ppd/dd/db/GetDefaultContentForDisplayTest.java
@@ -0,0 +1,41 @@
+package test.gov.fnal.ppd.dd.db;
+
+import static gov.fnal.ppd.dd.GlobalVariables.credentialsSetup;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import gov.fnal.ppd.dd.CredentialsNotFoundException;
+import gov.fnal.ppd.dd.db.GetDefaultContentForDisplay;
+
+public class GetDefaultContentForDisplayTest {
+	private GetDefaultContentForDisplay defaultContent = new GetDefaultContentForDisplay();
+
+	static {
+		try {
+			credentialsSetup();
+		} catch (CredentialsNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@Test
+	public void testGetChannelNumber() {
+		int d = defaultContent.getChannelNumber();
+		assertNotEquals(d, 0);
+	}
+
+	@Test
+	public void testGetDisplayType() {
+		String t = defaultContent.getDisplayType();
+		assertNotNull(t);
+		assertTrue(t.equals("Regular") || t.equals("FirefoxOnly"));
+	}
+
+	@Test
+	public void testGetDefaultURL() {
+		String u = defaultContent.getDefaultURL();
+		assertNotNull(u);
+	}
+
+}
