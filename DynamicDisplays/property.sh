31c5f7a87a5bfe31202aa7f41e93cb49525d3ec4
Elliott McCcrory <mccrory@fnal.gov>
2021-04-29 16:50:42 -0500

    Remove the trailing "\r" from the output of the property.sh script.

--- DynamicDisplays/property.sh
+++ DynamicDisplays/property.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-# Read configuration into an associative array
-declare -A CONFIG
-# IFS is the 'internal field separator'.
-IFS=" "
-while read -r key value
-do
-    CONFIG[$key]=$value
-done < config/config.properties
-unset IFS
-
-# If a parameter is passed, look it up by that, else exit
 if [ $1 ]; then
-    echo ${CONFIG[$1]}
+    # Read configuration into an associative array
+    declare -A CONFIG
+    # IFS is the 'internal field separator'.
+    IFS=" "
+    while read -r key value
+    do
+	CONFIG[$key]=$value
+    done < config/config.properties
+    unset IFS
+    
+    # Look up the parameters as passed as the first argument here
+    echo ${CONFIG[$1]} | sed 's/\r//g' # It always seems to end in \r
 fi
 
 exit $?
