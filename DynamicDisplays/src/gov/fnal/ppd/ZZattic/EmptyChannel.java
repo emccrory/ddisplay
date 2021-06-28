356af6aa14449a7ccf666f8f368db052b0d0e47c
Elliott McCcrory <mccrory@fnal.gov>
2021-06-28 11:38:22 -0500

    Remove the code so it doesn't ever again make an unnecessary compile error

--- DynamicDisplays/src/gov/fnal/ppd/ZZattic/EmptyChannel.java
+++ DynamicDisplays/src/gov/fnal/ppd/ZZattic/EmptyChannel.java
@@ -1,44 +1,9 @@
 package gov.fnal.ppd.ZZattic;
 
-import gov.fnal.ppd.dd.changer.ChannelClassification;
-import gov.fnal.ppd.dd.channel.ChannelImpl;
-
-import static gov.fnal.ppd.dd.util.specific.PackageUtilities.MY_NAME;
-import static gov.fnal.ppd.dd.util.specific.PackageUtilities.MY_URL;
-
-import java.net.URI;
-import java.net.URISyntaxException;
-
 /**
- * An empty channel for use by these XML classes - This used to be the name of the class, but it was changed to XmlDerivedChannel.
- * This class has been reintroduced so that channels stored in the table DefaultChannels will still work.
- * 
- * @author Elliott McCrory, Fermilab/AD/Instrumentation.
  * 
  * @deprecated
  */
-public class EmptyChannel extends ChannelImpl {
-	private static final long serialVersionUID = -419375552528263824L;
-
-	/**
-	 * Create an Empty Channel
-	 * 
-	 * @throws URISyntaxException
-	 *             (In the unlikely case that the default web page is no longer a valid URI)
-	 */
-	public EmptyChannel() throws URISyntaxException {
-		this(MY_NAME, ChannelClassification.MISCELLANEOUS);
-
-	}
-
-	public EmptyChannel(String string, ChannelClassification categ) throws URISyntaxException {
-		super(string, categ, "This channel is " + string, new URI(MY_URL), 0, 0);
-	}
-
-	/**
-	 * @param args
-	 */
-	public static void main(final String[] args) {
-		System.out.println(MY_URL);
-	}
+public class EmptyChannel {
+	// Code removed - see git repository for details.
 }
