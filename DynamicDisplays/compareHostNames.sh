3e10df1e8dba0c292459f6bb82fce821727af151
Elliott McCcrory <mccrory@fnal.gov>
2021-07-02 13:32:18 -0500

    Add a new script to compare the JVM reckoning of the hostname with the OS reckoning.  If these are different, there will be problems.

---
+++ DynamicDisplays/compareHostNames.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#
+# Shell script to orchestrate giving this computer a public/private key pair
+#
+
+. setupEnvironment.sh
+
+CLIENTNAME=$(hostname | sed 's/.fnal.gov//g' | sed 's/.local//g' )
+
+echo "The system name from the operating system for this computer is $CLIENTNAME"
+JVM_NAME=$(java gov.fnal.ppd.security.CheckHostName)
+echo "$JVM_NAME"
+
+n=$(echo "$JVM_NAME" | awk '{ print $9 }' | sed "s/'//g")
+
+if [ "$n" = "$CLIENTNAME" ]; then
+    echo These names match
+else
+    echo "These names DO NOT MATCH: JVM name is '$n', Operating system name is '$CLIENTNAME'"
+    exit 1;
+fi
--- DynamicDisplays/src/gov/fnal/ppd/security/package-info.java
+++ DynamicDisplays/src/gov/fnal/ppd/security/package-info.java
@@ -3,7 +3,8 @@
  * Fermilab's Dynamic Displays System -- Implementation of the classes for doing digital signatures.
  * </p>
  * <p>
- * This separate package is here to simplify the jar file needed for a user to generate a new public/private key pair.
+ * This separate package is here to simplify the jar file needed for a user to generate a new public/private key pair. Everything in
+ * this package should only depend on the things within this package - nowhere else.
  * </p>
  * 
  * @author Elliott McCrory, Fermilab AD/Instrumentation
