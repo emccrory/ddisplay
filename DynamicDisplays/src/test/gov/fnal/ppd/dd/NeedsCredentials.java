d5b7957b699d4ca6a7f856e208d4087617af8634
Elliott McCcrory <mccrory@fnal.gov>
2021-07-08 12:18:55 -0500

    Simplify some tests

--- DynamicDisplays/src/test/gov/fnal/ppd/dd/NeedsCredentials.java
+++ DynamicDisplays/src/test/gov/fnal/ppd/dd/NeedsCredentials.java
@@ -2,8 +2,6 @@ package test.gov.fnal.ppd.dd;
 
 import static gov.fnal.ppd.dd.GlobalVariables.credentialsSetup;
 
-import gov.fnal.ppd.dd.util.nonguiUtils.CredentialsNotFoundException;
-
 /**
  * Several tests in this part of the suite need to make a connection to the database. They should all extend this class so that the
  * static block, here, is executed. And then the classes that need the connection should check the value of credentialsOK. This code
@@ -19,7 +17,7 @@ public class NeedsCredentials {
 		try {
 			credentialsSetup();
 			credentialsOK = true;
-		} catch (CredentialsNotFoundException e) {
+		} catch (gov.fnal.ppd.dd.util.nonguiUtils.CredentialsNotFoundException e) {
 			e.printStackTrace();
 		}
 	}
--- DynamicDisplays/src/test/gov/fnal/ppd/dd/changer/ChannelCatalogFactoryTest.java
+++ DynamicDisplays/src/test/gov/fnal/ppd/dd/changer/ChannelCatalogFactoryTest.java
@@ -1,14 +1,13 @@
 package test.gov.fnal.ppd.dd.changer;
 
-import static gov.fnal.ppd.dd.GlobalVariables.credentialsSetup;
 import static gov.fnal.ppd.dd.changer.ChannelCatalogFactory.getInstance;
 import static gov.fnal.ppd.dd.changer.ChannelCatalogFactory.refresh;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 import static org.junit.Assume.assumeTrue;
 
 import org.junit.Test;
 
-import gov.fnal.ppd.dd.util.nonguiUtils.CredentialsNotFoundException;
 import test.gov.fnal.ppd.dd.NeedsCredentials;
 /**
  * Tests of the class ChannelCatalogFactory
@@ -19,27 +18,18 @@ import test.gov.fnal.ppd.dd.NeedsCredentials;
 public class ChannelCatalogFactoryTest extends NeedsCredentials {
 
 	@Test
+	public void testAnything() {
+		assertTrue(true);
+	}
+	
+	@Test
 	public void testGetInstance() {
-		try {
-			credentialsSetup();
-		} catch (CredentialsNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
 		assumeTrue(credentialsOK);
 		assertNotNull(getInstance());
 	}
 
 	@Test
 	public void testRefresh() {
-		try {
-			credentialsSetup();
-		} catch (CredentialsNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
 		assumeTrue(credentialsOK);
 		assertNotNull(refresh());
 	}
