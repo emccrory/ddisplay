bd45ef958e50a85d039b1c110ee506d6e602d21b
Elliott McCcrory <mccrory@fnal.gov>
2021-07-12 15:47:46 -0500

    Move this test class to the test package

--- DynamicDisplays/src/gov/fnal/ppd/dd/testing/FirstSeleniumTest.java
+++ DynamicDisplays/src/gov/fnal/ppd/dd/testing/FirstSeleniumTest.java
@@ -1,4 +1,4 @@
-package gov.fnal.ppd.dd.display.client.selenium;
+package gov.fnal.ppd.dd.testing;
 
 import static gov.fnal.ppd.dd.GetMessagingServer.getMessagingServerNameDisplay;
 import static gov.fnal.ppd.dd.GlobalVariables.credentialsSetup;
@@ -9,6 +9,7 @@ import java.awt.Color;
 import java.util.ArrayList;
 import java.util.List;
 
+import gov.fnal.ppd.dd.display.client.selenium.SeleniumConnectionToBrowser;
 import gov.fnal.ppd.dd.util.nonguiUtils.CredentialsNotFoundException;
 
 /**
