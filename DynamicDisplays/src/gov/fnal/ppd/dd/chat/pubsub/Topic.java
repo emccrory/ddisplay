8c21edbb2652f28adb5fa8ad5c3046041638ecbf
Elliott McCcrory <mccrory@fnal.gov>
2021-09-23 10:57:53 -0500

    Update the comments in this new package.

--- DynamicDisplays/src/gov/fnal/ppd/dd/chat/pubsub/Topic.java
+++ DynamicDisplays/src/gov/fnal/ppd/dd/chat/pubsub/Topic.java
@@ -2,6 +2,8 @@ package gov.fnal.ppd.dd.chat.pubsub;
 
 public class Topic {
 
+	// Maybe this class is not necessary - maybe only the Enum is required.
+
 	private TopicEnum name;
 
 	public Topic(TopicEnum name) {
--- DynamicDisplays/src/gov/fnal/ppd/dd/chat/pubsub/package-info.java
+++ DynamicDisplays/src/gov/fnal/ppd/dd/chat/pubsub/package-info.java
@@ -1,8 +1,15 @@
 /**
  * <p>
- * Fermilab's Dynamic Displays System -- First attempt, copied from the Internet, to do Publish/Subscribe
+ * Fermilab's Dynamic Displays System -- A clean and simple attempt, copied from the Internet, to do Publish/Subscribe
  * </p>
  * 
+ * <p>
+ * This was created September 2021. Note that the first attempt was implemented by hand, by me, in the MessagingServer, back in like
+ * 2018. It is likely that that first attempt was not a great implementation.
+ * </p>
+ * <p>
+ * This is untested. And it is not sophisticated enough to use for our system, but it might be a start.
+ * </p>
  * <p>
  * These files were taken from
  * <a href="https://riptutorial.com/design-patterns/example/6498/publish-subscribe-in-java">riptutorial.comn</a> on 09/23/2021.</a>
